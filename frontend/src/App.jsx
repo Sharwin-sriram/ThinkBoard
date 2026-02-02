f9e10d81a2faf2a669020f3a126e6f804892762f
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-02 16:03:38 +0530

    added toaster and router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import HomePage from "@pages/HomePage";
-import CreatePage from "@pages/CreatePage";
 import NoteDetailsPage from "@pages/NoteDetailsPage";
-
 import { Route, Routes } from "react-router";
+import CreatePage from "@pages/CreatePage";
+import HomePage from "@pages/HomePage";
+import toast from "react-hot-toast";
 
 export default () => {
   return (
-    <Routes>
-      <Route path={"/"} element={<HomePage />} />
-      <Route path={"/create"} element={<CreatePage />} />
-      <Route path={"/note/:id"} element={<NoteDetailsPage />} />
-    </Routes>
+    <>
+      <button onClick={() => toast.success("Congrats")}>Click</button>
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/create"} element={<CreatePage />} />
+        <Route path={"/note/:id"} element={<NoteDetailsPage />} />
+      </Routes>
+    </>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Toaster position="bottom-right" reverseOrder={false} />
     <App />
   </BrowserRouter>,
 );
