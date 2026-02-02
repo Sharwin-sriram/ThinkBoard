7dc66944ca48974b8576b01dc39f9d8a3bfc9553
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-02 13:00:15 +0530

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import Header from "@components/Header.jsx";
+import HomePage from "@pages/HomePage";
+import CreatePage from "@pages/CreatePage";
+import NoteDetailsPage from "@pages/NoteDetailsPage";
+
+import { Route, Routes } from "react-router";
 
 export default () => {
   return (
-    <>
-      <Header />
-      <Header />
-    </>
+    <Routes>
+      <Route path={"/"} element={<HomePage />} />
+      <Route path={"/create"} element={<CreatePage />} />
+      <Route path={"/note/:id"} element={<NoteDetailsPage />} />
+    </Routes>
   );
 };
