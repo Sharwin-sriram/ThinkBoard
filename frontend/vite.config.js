f28d1378187ef1922d0e6ea75cb00b5e1fad6a7e
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-02 12:54:22 +0530

    update vite config and jsconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
       "@styles": path.resolve(__dirname, "src/styles"),
+      "@pages": path.resolve(__dirname, "src/pages"),
     },
   },
 });
