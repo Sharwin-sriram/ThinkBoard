74b827456887f1697fb3078f166ed62f4931c1f9
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-04 16:08:10 +0530

    install cors on backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,19 @@
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 import express from "express";
 import connectDB from "./config/db.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
 
