9822fe8082ba62844626384817b16a138e125b98
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-06 15:22:00 +0530

    Add fetching logic to note details page

--- frontend/src/pages/NoteDetailsPage.jsx
+++ frontend/src/pages/NoteDetailsPage.jsx
@@ -1,3 +1,31 @@
+import { Link, useNavigate, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import api from "@lib/axios";
+import toast from "react-hot-toast";
+import { ArrowLeftIcon, LoaderIcon, Trash2Icon } from "lucide-react";
+
 export default () => {
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/${id}`);
+        setNote(res.data);
+      } catch (er) {
+        console.log("Error while fetching: ", er);
+        toast.error("Failed to fetch the note");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNote();
+  });
+
   return <div></div>;
 };
