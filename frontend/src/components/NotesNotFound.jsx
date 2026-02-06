e038ad4ecc19941785fa3b4da24812c268ded849
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-06 12:01:09 +0530

    create notes not found file

---
+++ frontend/src/components/NotesNotFound.jsx
@@ -0,0 +1,8 @@
+export default () => {
+  return (
+    <div className="text-center py-10">
+      <h2 className="text-xl font-semibold">No Notes Found</h2>
+      <p className="opacity-70">Create your first Note</p>
+    </div>
+  );
+};
