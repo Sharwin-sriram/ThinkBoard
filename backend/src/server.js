4ff89eb8cd9358cd4a57a16636499f56c828a36b
Sharwin-sriram <shreeramtooo@gmail.com>
2026-02-23 16:16:17 +0530

    build commands

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,30 @@ import express from "express";
 import connectDB from "./config/db.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
+}
+
 app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(5001, () => {
